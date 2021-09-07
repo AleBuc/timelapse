0e14bcd292fed478ee59d73bffafa97050d80f68
AlexandreBuc <koneochester@gmail.com>
2021-09-07 16:46:35 +0200

    feat: add image capture function

---
+++ timelapse.py
@@ -0,0 +1,14 @@
+from time import sleep
+from picamera import PiCamera
+
+totalTime = 60  # set the total time of capture in minutes
+capturePeriod = 10  # set the period in seconds between 2 taken pictures
+
+numPics = int((totalTime * 60) / capturePeriod)  # number of pictures to take
+
+camera = PiCamera()
+camera.resolution = (1920, 1080)
+
+for i in range(numPics):
+    camera.capture('/home/pi/Pictures/image'+str(i)+'.jpg')
+    sleep(capturePeriod)
