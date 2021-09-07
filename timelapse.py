f4e01ade56326019c1d42e780d92ace49daff0ed
AlexandreBuc <koneochester@gmail.com>
2021-09-07 17:30:03 +0200

    feat: add logs

--- timelapse.py
+++ timelapse.py
@@ -1,3 +1,4 @@
+import logging
 from time import sleep
 from picamera import PiCamera
 
@@ -5,6 +6,7 @@ totalTime = 60  # set the total time of capture in minutes
 capturePeriod = 10  # set the period in seconds between 2 taken pictures
 
 numPics = int((totalTime * 60) / capturePeriod)  # number of pictures to take
+logging.info(str(numPics) + " pictures to take.")
 
 camera = PiCamera()
 camera.resolution = (1920, 1080)
@@ -12,3 +14,4 @@ camera.resolution = (1920, 1080)
 for i in range(numPics):
     camera.capture('/home/pi/Pictures/image'+str(i)+'.jpg')
     sleep(capturePeriod)
+logging.info(str(range) + " taken pictures.")
