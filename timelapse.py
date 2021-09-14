aae15bce331161dc55c0303098c12f3b9136b255
AlexandreBuc <koneochester@gmail.com>
2021-09-14 21:48:39 +0200

    fix: reduce the precision of image numeration

--- timelapse.py
+++ timelapse.py
@@ -20,6 +20,6 @@ picsFolder = picturesDirectory + '/picsTaking' + date
 system('mkdir ' + picsFolder)
 
 for i in range(numPics):
-    camera.capture(picsFolder + '/img{0:08d}.png'.format(i), format='png')
+    camera.capture(picsFolder + '/img{0:05d}.png'.format(i), format='png')
     sleep(capturePeriod)
 logging.info(str(range) + " taken pictures.")
